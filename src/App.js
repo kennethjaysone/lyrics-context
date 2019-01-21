3ee94bab0bd894b3e9bc57f8c60eb457dab0e427
Kenneth Jaysone Francis <kennethjaysone@gmail.com>
2019-01-22 06:14:05 +0800

    Added Index for main container of layouts as well as Tracks component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import Navbar from "./components/layouts/Navbar";
+import Index from "./components/layouts/Index";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
     );
   }
 }
