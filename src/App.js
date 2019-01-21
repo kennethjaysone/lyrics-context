f4faaf3056f64005f062ae5073462a50519818e2
Kenneth Jaysone Francis <kennethjaysone@gmail.com>
2019-01-22 05:48:57 +0800

    added basic navbar within new folders for components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Navbar from "./components/layouts/Navbar";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <Navbar />
       </div>
     );
   }
